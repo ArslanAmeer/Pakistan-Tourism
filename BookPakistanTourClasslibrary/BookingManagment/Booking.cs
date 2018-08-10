5cb73bab87d8b40e01855272a4f55ca64a2ae55b
Arslan Ameer <arslan.ameer@hotmail.com>
2018-08-11 04:14:44 +0500

    🚧 ✨ Add: Booking Entity Class and Handler Class

---
+++ BookPakistanTourClasslibrary/BookingManagment/Booking.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookPakistanTourClasslibrary.TourManagement;
+using BookPakistanTourClasslibrary.UserManagement;
+
+namespace BookPakistanTourClasslibrary.BookingManagment
+{
+    public class Booking
+    {
+        [Key]
+        public int Id { get; set; }
+
+        //[Required]
+        //public string Name { get; set; }
+
+        //[Required]
+        //public float Phone { get; set; }
+
+        //public string Email { get; set; }
+
+        //[Required]
+        //public string Address { get; set; }
+
+        public User User { get; set; }
+
+        public Tour Tour { get; set; }
+
+    }
+}
---
+++ BookPakistanTourClasslibrary/BookingManagment/BookingHandler.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookPakistanTourClasslibrary.BookingManagment
+{
+    public class BookingHandler
+    {
+        private readonly DbContextClass _db = new DbContextClass();
+
+        public List<Booking> GetAllBookings()
+        {
+            using (_db)
+            {
+                return (from b in _db.Bookings
+                    .Include(b => b.Tour)
+                        select b).ToList();
+            }
+        }
+
+        public Booking GetBookingById(int id)
+        {
+            using (_db)
+            {
+                return (from b in _db.Bookings
+                        .Include(b => b.Tour)
+                        where b.Id == id
+                        select b).FirstOrDefault();
+            }
+        }
+
+        public List<Booking> GetBookingsByUserId(int userId)
+        {
+            using (_db)
+            {
+                return (from b in _db.Bookings
+                        .Include(b => b.Tour)
+                        where b.User.Id == userId
+                        select b).ToList();
+            }
+        }
+
+        public List<Booking> GetBookingsByTourId(int tourId)
+        {
+            using (_db)
+            {
+                return (from b in _db.Bookings
+                        .Include(b => b.Tour)
+                        where b.Tour.Id == tourId
+                        select b).ToList();
+            }
+        }
+    }
+}
