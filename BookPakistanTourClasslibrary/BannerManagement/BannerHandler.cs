cbaca34fce4fb0e08678c521b3ddd2c46908274b
Arslan Ameer <arslan.ameer@hotmail.com>
2018-08-11 02:55:04 +0500

    🚧 Add: Banners Entity Class and Handler

---
+++ BookPakistanTourClasslibrary/BannerManagement/BannerHandler.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookPakistanTourClasslibrary.BannerManagement
+{
+    public class BannerHandler
+    {
+        private readonly DbContextClass _db = new DbContextClass();
+
+        public List<MainBanner> GetAllBanners()
+        {
+            using (_db)
+            {
+                return (from b in _db.MainBanners select b).ToList();
+            }
+        }
+
+        public MainBanner Getbanner(int? id)
+        {
+            using (_db)
+            {
+                return (from b in _db.MainBanners where b.Id == id select b).FirstOrDefault();
+            }
+        }
+
+        public void AddBanner(MainBanner banner)
+        {
+            using (_db)
+            {
+                _db.MainBanners.Add(banner);
+                _db.SaveChanges();
+            }
+        }
+
+        public void UpdateBanner(MainBanner banner)
+        {
+            using (_db)
+            {
+                _db.Entry(banner).State = EntityState.Modified;
+                _db.SaveChanges();
+            }
+        }
+
+        public void DeleteBanner(int id)
+        {
+            using (_db)
+            {
+                _db.MainBanners.Remove(_db.MainBanners.Find(id) ?? throw new InvalidOperationException());
+                _db.SaveChanges();
+            }
+        }
+
+        public int GetBannerCount()
+        {
+            using (_db)
+            {
+                return (from c in _db.MainBanners select c).Count();
+            }
+        }
+
+    }
+}
---
+++ BookPakistanTourClasslibrary/BannerManagement/MainBanner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookPakistanTourClasslibrary.BannerManagement
+{
+    public class MainBanner
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Caption { get; set; }
+
+        public string Banner_Url { get; set; }
+
+    }
+}
