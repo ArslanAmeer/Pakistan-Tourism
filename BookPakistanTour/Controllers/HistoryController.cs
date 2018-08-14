7c6ee63635461c7725a2f11112846744407d8224
Arslan Ameer <arslan.ameer@hotmail.com>
2018-08-14 17:19:13 +0500

    ✨👌 Add: History Managment Fully Functional Added

---
+++ BookPakistanTour/Controllers/HistoryController.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BookPakistanTourClasslibrary;
+using BookPakistanTourClasslibrary.HistoryManagement;
+using BookPakistanTourClasslibrary.UserManagement;
+
+namespace BookPakistanTour.Controllers
+{
+    public class HistoryController : Controller
+    {
+        private DbContextClass db = new DbContextClass();
+
+        // GET: History
+        public ActionResult Index()
+        {
+            User u = (User)Session[WebUtil.CURRENT_USER];
+            if (!(u != null && u.IsInRole(WebUtil.ADMIN_ROLE)))
+            {
+                return RedirectToAction("Login", "User", new { ctl = "Admin", act = "AdminPanel" });
+            }
+            return View(db.Histories.ToList());
+        }
+
+        // GET: History/Details/5
+        public ActionResult Details(int? id)
+        {
+            User u = (User)Session[WebUtil.CURRENT_USER];
+            if (!(u != null && u.IsInRole(WebUtil.ADMIN_ROLE)))
+            {
+                return RedirectToAction("Login", "User", new { ctl = "Admin", act = "AdminPanel" });
+            }
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            History history = db.Histories.Find(id);
+            if (history == null)
+            {
+                return HttpNotFound();
+            }
+            return View(history);
+        }
+
+        // GET: History/Create
+        public ActionResult Create()
+        {
+            User u = (User)Session[WebUtil.CURRENT_USER];
+            if (!(u != null && u.IsInRole(WebUtil.ADMIN_ROLE)))
+            {
+                return RedirectToAction("Login", "User", new { ctl = "Admin", act = "AdminPanel" });
+            }
+            return View();
+        }
+
+        // POST: History/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Title,Description,ImageUrl")] History history, FormCollection fdata)
+        {
+            User u = (User)Session[WebUtil.CURRENT_USER];
+            if (!(u != null && u.IsInRole(WebUtil.ADMIN_ROLE)))
+            {
+                return RedirectToAction("Login", "User", new { ctl = "Admin", act = "AdminPanel" });
+            }
+            if (ModelState.IsValid)
+            {
+                long numb = DateTime.Now.Ticks;
+                int count = 0;
+                foreach (string fname in Request.Files)
+                {
+                    HttpPostedFileBase file = Request.Files[fname];
+                    if (!string.IsNullOrEmpty(file?.FileName))
+                    {
+                        string url = "/ImagesData/HistoryImages/" + numb + "_" + ++count + file.FileName.Substring(file.FileName.LastIndexOf(".", StringComparison.Ordinal));
+                        string path = Request.MapPath(url);
+                        file.SaveAs(path);
+                        history.ImageUrl = url;
+                    }
+                    else
+                    {
+                        string url = "/ImagesData/HistoryImages/noimage2.jpg";
+                        history.ImageUrl = url;
+                    }
+                }
+
+                db.Histories.Add(history);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(history);
+        }
+
+        // GET: History/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            User u = (User)Session[WebUtil.CURRENT_USER];
+            if (!(u != null && u.IsInRole(WebUtil.ADMIN_ROLE)))
+            {
+                return RedirectToAction("Login", "User", new { ctl = "Admin", act = "AdminPanel" });
+            }
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            History history = db.Histories.Find(id);
+            if (history == null)
+            {
+                return HttpNotFound();
+            }
+            return View(history);
+        }
+
+        // POST: History/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Title,Description,ImageUrl")] History history)
+        {
+            User u = (User)Session[WebUtil.CURRENT_USER];
+            if (!(u != null && u.IsInRole(WebUtil.ADMIN_ROLE)))
+            {
+                return RedirectToAction("Login", "User", new { ctl = "Admin", act = "AdminPanel" });
+            }
+            if (ModelState.IsValid)
+            {
+                db.Entry(history).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(history);
+        }
+
+        // GET: History/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            User u = (User)Session[WebUtil.CURRENT_USER];
+            if (!(u != null && u.IsInRole(WebUtil.ADMIN_ROLE)))
+            {
+                return RedirectToAction("Login", "User", new { ctl = "Admin", act = "AdminPanel" });
+            }
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            History history = db.Histories.Find(id);
+            if (history == null)
+            {
+                return HttpNotFound();
+            }
+            return View(history);
+        }
+
+        // POST: History/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            User u = (User)Session[WebUtil.CURRENT_USER];
+            if (!(u != null && u.IsInRole(WebUtil.ADMIN_ROLE)))
+            {
+                return RedirectToAction("Login", "User", new { ctl = "Admin", act = "AdminPanel" });
+            }
+            History history = db.Histories.Find(id);
+            db.Histories.Remove(history);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public int GetHistoryCount()
+        {
+            return db.Histories.Count();
+        }
+    }
+}
---
+++ BookPakistanTourClasslibrary/HistoryManagement/History.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookPakistanTourClasslibrary.HistoryManagement
+{
+    public class History
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+
+    }
+}
