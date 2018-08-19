f7941540189ca0ea8a52b29dcbeb6f1a054e0e9e
Arslan Ameer <arslan.ameer@hotmail.com>
2018-08-19 05:07:37 +0500

    ♻👌 Update: Booking Managment and Delete for Admin

--- BookPakistanTour/Controllers/BookingController.cs
+++ BookPakistanTour/Controllers/BookingController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using BookPakistanTourClasslibrary;
@@ -10,9 +12,9 @@ using BookPakistanTourClasslibrary.UserManagement;
 
 namespace BookPakistanTour.Controllers
 {
-    [OutputCache(NoStore = true, Duration = 0, VaryByParam = "*")]
     public class BookingController : Controller
     {
+        private readonly DbContextClass db = new DbContextClass();
         // GET: Booking
         public ActionResult BookTrip(int id)
         {
@@ -39,12 +41,71 @@ namespace BookPakistanTour.Controllers
             Booking booking = new Booking
             {
                 Tour = tour,
-                User = user
+                UserId = user.Id
             };
 
             new BookingHandler().AddBooking(booking);
             ViewBag.HideSlider = true;
             return View();
+        }
+
+        public ActionResult BookingList()
+        {
+            List<Booking> bookings = new BookingHandler().GetAllBookings();
+            return View(bookings);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            User u = (User)Session[WebUtil.CURRENT_USER];
+            if (!(u != null && u.IsInRole(WebUtil.ADMIN_ROLE)))
+            {
+                return RedirectToAction("Login", "User", new { ctl = "Admin", act = "AdminPanel" });
+            }
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Booking booking = new BookingHandler().GetBookingById(id);
+            if (booking == null)
+            {
+                return HttpNotFound();
+            }
+            return View(booking);
+        }
+
+        // POST: History/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            User u = (User)Session[WebUtil.CURRENT_USER];
+            if (!(u != null && u.IsInRole(WebUtil.ADMIN_ROLE)))
+            {
+                return RedirectToAction("Login", "User", new { ctl = "Admin", act = "AdminPanel" });
+            }
+            Booking booking = new BookingHandler().GetBookingById(id);
+            db.Entry(booking.Tour).State = EntityState.Unchanged;
+            db.Entry(booking.User).State = EntityState.Unchanged;
+            db.Entry(booking).State = EntityState.Deleted;
+            db.Bookings.Remove(booking);
+            db.SaveChanges();
+            return RedirectToAction("BookingList");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public int GetBokingCount()
+        {
+            return (new BookingHandler().BookingCount());
         }
     }
 }
